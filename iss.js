3609bd36cb395045c857ce3636026de6a5b41587
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-18 23:21:42 -0400

    define fetchCoordsByIP, pass test in index.js

--- iss.js
+++ iss.js
@@ -25,7 +25,31 @@ const fetchMyIP = (callback) => {
   });
 };
 
-const fetchCoordsByIP = (ip, callback) => {};
+const fetchCoordsByIP = (ip, callback) => {
+  const url = `http://ipwho.is/${ip}`;
+  //use request to fetch coordinates from ipwhois API
+  request(url, (error, response, body) => {
+    // on error, pass to callback
+    if (error) {
+      callback(error, null);
+      return;
+      // if non-200 status, assume server error and pass error to callback
+    } else if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when attempting to fetch ip coordinates.`;
+      callback(Error(msg), null);
+      return;
+      // if successful, pass coordinates to callback
+    } else if (response.statusCode === 200) {
+      const data = JSON.parse(body);
+      const latitude = data.latitude;
+      const longitude = data.longitude;
+      const coordinates = {
+        latitude,
+        longitude,
+      };
+      callback(null, coordinates);
+    }
+  });
+};
 
-
-module.exports = { fetchMyIP, fetchCoordsByIP};
+module.exports = { fetchMyIP, fetchCoordsByIP };
