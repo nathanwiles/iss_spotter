6f7fbefd83653fa28669582c774e37fa64a5835a
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-20 07:00:18 -0400

    call fetchMyIP

--- index2.js
+++ index2.js
@@ -1,3 +1,7 @@
 // Created by: Nathan Wiles
 // Part of ISS Spotter With Promises project
 
+const { fetchMyIP } = require("./iss_promised");
+
+fetchMyIP()
+.then((data) => {console.log(data)}, (error) => {console.log(error)});
