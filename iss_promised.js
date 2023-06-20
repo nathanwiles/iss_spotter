37428b57375a2e0c2d49e10dfe31da848ee9078f
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-20 07:20:07 -0400

    build fetchCoordsByIP

--- iss_promised.js
+++ iss_promised.js
@@ -9,8 +9,15 @@ const issURL = "https://iss-flyover.herokuapp.com/json/?";
 
 // function to fetch IP address from ipify API
 const fetchMyIP = () => {
-  return request("https://api.ipify.org?format=json");
+  return request(ipifyURL);
 };
 
+// function to fetch coordinates from ipwhois API
+const fetchCoordsByIP = (body) => {
+  const data = JSON.parse(body);
+  const ip = data.ip;
+  return request(ipwhoisURL + ip);
+}
 
-module.exports = { fetchMyIP };
+
+module.exports = { fetchMyIP, fetchCoordsByIP};
