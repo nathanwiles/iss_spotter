63c81c76d6da1efe93d2fa553b8911cf7700643d
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-19 22:51:00 -0400

    call function and pass callback

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
+const { nextISSTimesForMyLocation } = require("./iss");
+
+// function to retrieve and print flyover times
+nextISSTimesForMyLocation((error, data) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  } else {
+    if (data) {
+      console.log("It worked! Returned flyover times:");
+      for (const i in data) {
+        console.log(data[i]);
+      }
+    }
+  }
+});
 
 /* test code
+
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
+
+
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!", error);
@@ -12,7 +31,7 @@ fetchMyIP((error, ip) => {
 });
 
 
-fetchCoordsByIP("9", (error, data) => {
+fetchCoordsByIP("9.9.9.9", (error, data) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
