ab1a7df11864140d83db73fcd939c07c29b4c212
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-20 06:20:39 -0400

    create files

---
+++ index2.js
@@ -0,0 +1,3 @@
+// Created by: Nathan Wiles
+// Part of ISS Spotter With Promises project
+
---
+++ iss_promised.js
@@ -0,0 +1,5 @@
+// Created by: Nathan Wiles
+// Part of ISS Spotter With Promises project
+
+const request = require("request-promise-native");
+
