064a60503d8c7d3eeddff9f4f9029d622b85799c
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-21 19:31:32 -0400

    call nextISSTimesForMyLocation and handle data.

--- index2.js
+++ index2.js
@@ -1,12 +1,20 @@
 // Created by: Nathan Wiles
 // Part of ISS Spotter With Promises project
 
-const { fetchMyIP, fetchCoordsByIP } = require("./iss_promised");
+const { nextISSTimesForMyLocation } = require("./iss_promised");
 
-fetchMyIP()
-  .then((body) => fetchCoordsByIP(body))
-  .then(
-    (data) => {
-      console.log(data);
-    },
-  );
+const printPassTimes = (passTimes) => {
+  for (const pass of passTimes) {
+    console.log(pass);
+  }
+};
+
+
+nextISSTimesForMyLocation()
+.then((passTimes) => {
+  printPassTimes(passTimes);
+})
+.catch((error) => {
+  console.log("It didn't work: ", error.message);
+});
+
